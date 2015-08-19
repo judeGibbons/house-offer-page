1c55db2c7f9059414e1fa61b8409cf1ed8a84d29
Jude Gibbons <judes@gimaju.net>
2015-08-19 19:15:10 +0100

    Second commit: jQuery toggle arrows added

--- js/purplebricks.js
+++ js/purplebricks.js
@@ -1,4 +1,20 @@
 $( document ).ready(function() {
 
+ $( "body" ).removeClass( "no-js" );
 
+ $( ".toggle" ).click(function() {
+ 	var target = $( event.target );
+  	var affectedContent = target.parent().siblings()[0];
+  	//var toggleText = $(".toggle").text();
+  	var toggleText = target.text();
+  	var toggleArrow = "<span class='arrow'></span>";
+  	var toggleArrowClosed = "<span class='arrow closed'></span>";
+  	$(affectedContent).slideToggle( "fast", function() {
+  	  if (toggleText == "hide") {
+  	  	target.html("show"+toggleArrowClosed);
+  	  } else if (toggleText == "show") {
+  	  	target.html("hide"+toggleArrow);
+  	  };
+	});
+  });
 });
