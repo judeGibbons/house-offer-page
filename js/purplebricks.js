7bc4834e03374e9529d93b035dc77348ff80d0ca
Jude Gibbons <judes@gimaju.net>
2015-08-19 21:27:40 +0100

    Third commit: new offer price validated against asking price and logged as an object.

--- js/purplebricks.js
+++ js/purplebricks.js
@@ -17,4 +17,39 @@ $( document ).ready(function() {
   	  };
 	});
   });
+
+/* =============== doesn't work yet! ===================
+  function insertCommas() {
+	var charsInNewOfferPrice = 0;
+	$( "input#offerPrice" ).keypress(function() {
+	  charsInNewOfferPrice++;
+	  if ((charsInNewOfferPrice>1) && (charsInNewOfferPrice%3 == 1)) {
+	  console.log(charsInNewOfferPrice);
+
+
+	  $("input#offerPrice").val(',' + $("input#offerPrice").val());
+	  }; 	  
+	});
+  };
+  insertCommas();
+===================================================== */
+
+  $( "form" ).submit(function( event ) {
+    var newOfferPrice = +$( "input#offerPrice" ).val().replace(/[^\d\.]/g, '');
+    event.preventDefault();
+    console.log(JSON.stringify($("input#offerPrice").serializeArray()));
+    validation(newOfferPrice);
+  });
+
+
+  function validation(newOfferPrice) {
+    var askingPrice = +$("#askingPrice").text().replace(/[^\d\.]/g, '');
+	  $(askingPrice).val(askingPrice);
+	  if (newOfferPrice >= askingPrice) {
+	  	$("#validationMessage").text("Your offer matches or exceeds the asking price.");
+	  } else {
+	  	$("#validationMessage").text("Error: your offer is less than the asking price.");
+	  };
+	};
+
 });
